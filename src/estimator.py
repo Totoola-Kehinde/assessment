a42edc4aa190f514c85131b7eeda37a4355aa4a6
Totoola Kenny <40864358+Totoola-Kehinde@users.noreply.github.com>
2020-04-09 23:06:00 +0100

    first commit

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,33 @@
+region = {}
+name = ""
+avgAge = ""
+avgDailyIncomeInUSD = ""
+avgDailyIncomePopulation = ""
+periodType = ""
+timeToElapse = ""
+reportedCases = ""
+population = ""
+totalHospitalBeds = ""
+
+data = {
+  region: {
+  name: "Africa",
+  avgAge: 19.7,
+  avgDailyIncomeInUSD: 5,
+  avgDailyIncomePopulation: 0.71
+  },
+  periodType: "days",
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+}
+
+def covid19ImpactEstimator(data):
+  # get currently infected cases
+  currentlyInfected = data.reportedCases * 10
+  return currentlyInfected
+
 def estimator(data):
   return data
+
